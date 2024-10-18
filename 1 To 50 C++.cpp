8d1f678122d8e4956ab7796e0352daca83191d44
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-19 00:21:44 +0300

    Update 1 To 50 C++.cpp

--- 1 To 50 C++.cpp
+++ 1 To 50 C++.cpp
@@ -1565,3 +1565,60 @@ int main()
 //================================================================================================================================================================
 //================================================================================================================================================================
 
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+struct strMoney
+{
+    int Penny;
+    int Nickel;
+    int Dime;
+    int Quarter;
+    int Dollar;
+};
+
+strMoney ReadMony()
+{
+    strMoney MonyPNDQD;
+
+    cout << "Enter Your Total Penny : ";
+    cin >> MonyPNDQD.Penny;
+    cout << "Enter Your Total Nickel : ";
+    cin >> MonyPNDQD.Nickel;
+    cout << "Enter Your Total Dime : ";
+    cin >> MonyPNDQD.Dime;
+    cout << "Enter Your Total Quarter : ";
+    cin >> MonyPNDQD.Quarter;
+    cout << "Enter Your Total Dollar : ";
+    cin >> MonyPNDQD.Dollar;
+
+    return MonyPNDQD;
+}
+
+int CalcuMonyPNDQD(strMoney MonyPNDQD)
+{
+    int TotalPennies = 0;
+    TotalPennies = (MonyPNDQD.Penny * 1) + (MonyPNDQD.Nickel * 5) + (MonyPNDQD.Dime * 10) + (MonyPNDQD.Quarter * 25) + (MonyPNDQD.Dollar * 100);
+    return TotalPennies;
+}
+
+
+int main()
+{
+    int TotalPennies, TotalDollar;
+
+    TotalPennies = CalcuMonyPNDQD(ReadMony());
+    //TotalDollar = CalcuMonyPNDQD(ReadMony()) / 100;
+
+    cout << "\nTotal Pennies is : "<< TotalPennies <<endl;
+    cout << "\nTotal Pennies is : " << TotalPennies /100 << endl;
+
+    return 0;
+}
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
