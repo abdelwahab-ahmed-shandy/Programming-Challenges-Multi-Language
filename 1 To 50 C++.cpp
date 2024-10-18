40e1d9bcb45651196e0fe01f6a19683f1595403c
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-18 03:15:58 +0300

    Update 1 To 50 C++.cpp

--- 1 To 50 C++.cpp
+++ 1 To 50 C++.cpp
@@ -330,3 +330,100 @@ int main()
 //================================================================================================================================================================
 //================================================================================================================================================================
 
+// Problem_9 >>  Write a program to ask the user to enter : Number1, Number2, Number3 Then print the Sum of entered numbers 
+
+#include <iostream>
+using namespace std;
+
+void ReadNumbers(int Arry[3])
+{
+	cout << "Write a program to ask the user to enter : Number1, Number2, Number3 Then print the Sum of entered numbers" << endl << endl;
+
+	cout << "Enter Number 1 : ";
+	cin >> Arry[0];
+
+	cout << "Enter Number 2 : ";
+	cin >> Arry[1];
+
+	cout << "Enter Number 3 : ";
+	cin >> Arry[2];
+}
+
+int SumNumbers(int Arry[3])
+{
+	int Sum;
+
+	Sum = Arry[0] + Arry[1] + Arry[2];
+
+	return Sum;
+}
+
+void PrintSumNumbers(int Sum)
+{
+	cout << "\n====================================\n";
+	cout << "The Sum Of Numbers is : " << Sum << endl;
+	cout << "====================================\n";
+}
+
+int main()
+{
+	int Arry[3];
+	ReadNumbers(Arry);
+	PrintSumNumbers(SumNumbers(Arry));
+	return 0;
+}
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+Problem_10 >> Write a program to ask the user to enter : Mark1 , Mark2 , Mark3 Then print the average of entered Marks
+
+#include <iostream>
+using namespace std;
+
+void ReadMarks(float & Mark1 ,float & Mark2, float & Mark3)
+{
+	cout << "Write a program to ask the user to enter : Mark1 , Mark2 , Mark3 Then print the average of entered Marks" << endl << endl;
+
+	cout << "Enter Mark 1 : ";
+	cin >> Mark1;
+
+	cout << "Enter Mark 2 : ";
+	cin >> Mark2;
+
+	cout << "Enter Mark 3 : ";
+	cin >> Mark3;
+}
+
+float SumMarks(float Mark1,float Mark2,float Mark3)
+{
+	return Mark1 + Mark2 + Mark3;
+}
+
+float AvergeCalc(float Mark1, float Mark2, float Mark3)
+{
+	return SumMarks(Mark1, Mark2, Mark3) / 3;
+}
+
+void PrintResutAverge(float Result)
+{
+	cout << "\n====================================\n";
+	cout << "The Average Of Marks is : " << Result << endl;
+	cout << "====================================\n";
+}
+
+int main()
+{
+	float Mark1, Mark2 , Mark3;
+	ReadMarks(Mark1, Mark2, Mark3);
+	PrintResutAverge(AvergeCalc(Mark1, Mark2, Mark3));
+
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
