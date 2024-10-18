ebba8ae3772fa3c6555a37f1547d39fe3ae73eaf
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-18 09:40:07 +0300

    Update 1 To 50 C++.cpp

--- 1 To 50 C++.cpp
+++ 1 To 50 C++.cpp
@@ -905,4 +905,245 @@ int main()
 //================================================================================================================================================================
 //================================================================================================================================================================
 
+// Problem_21 >> Circle Area Along the circumference 
+// Write a program to calculate circle area along the circumference, then print it on the screen 
+// Area = (L * L) / (4 * PI)
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float ReadLongCircumference()
+{
+	cout << "Write a program to calculate circle area along the circumference, then print it on the screen" << endl << endl;
+	float L;
+	cout << "Enter Along The Circumference : ";
+	cin >> L;
+	return L;
+}
+
+float CalcuCircleAreaLongCircumference(float L)
+{
+	const float PI = 3.14159265359;
+
+	float Result = (pow(L, 2)) / (4 * PI);
+	return Result;
+}
+
+void PrintCircleArea(float CircleArea)
+{
+	cout << "\n==========================\n";
+	cout << "The Circle Area is  : " << CircleArea << endl;
+	cout << "==========================\n";
+}
+
+
+int main()
+{
+	PrintCircleArea(CalcuCircleAreaLongCircumference(ReadLongCircumference()));
+	return 0;
+}
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+//Problem_22 >> Circle Area Inscribed in an Isosceles Triangle
+//Write a program to calculate circle area Inscribed in an Isosceles Triangle, Then print it on the screen
+// Area = (PI*b*b/4)*((2*a-b)/(2*a+b))
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadSideBase(float &A, float &B)
+{
+	cout << "Write a program to calculate circle area Inscribed in an Isosceles Triangle, Then print it on the screen" << endl << endl;
+	cout << "Enter The Triangle side : ";
+	cin >> A;
+	cout << "Enter The Triangle Base : ";
+	cin >> B;
+}
+
+float CalcucircleAreaTriangle(float A , float B)
+{
+	const float PI = 3.14159265359;
+
+	float Result = PI * (pow(B, 2) / 4) * ((2 * A - B) / (2 * A + B) );
+	return Result;
+}
+
+void PrintCircleArea(float CircleArea)
+{
+	cout << "\n==========================\n";
+	cout << "The Circle Area is  : " << CircleArea << endl;
+	cout << "==========================\n";
+}
+
+
+int main()
+{
+	float A, B;
+	ReadSideBase(A, B);
+	PrintCircleArea(CalcucircleAreaTriangle(A,B));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+
+// Problem_23 >> Circle Area Circle Described Around an Arbitrary Triangle
+// Write a program to calculate Circle Described Around an Arbitrary Triangle ,Then print it on the screen .
+// T = (a*b*c) / (4*sqrt (P*(P-a)*(P-b)*(P-c)))
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadRightSideBase(float &A, float &B, float &C)
+{
+	cout << "Write a program to calculate Circle Described Around an Arbitrary Triangle ,Then print it on the screen" << endl << endl;
+	cout << "Enter The Right Side : ";
+	cin >> A;
+	cout << "Enter The Triangle Base : ";
+	cin >> B;
+	cout << "Enter The Triangle Side : ";
+	cin >> C;
+}
+
+float CalcuCircleAreaTriangle(float A , float B , float C)
+{
+	const float PI = 3.14159265359;
+	float P = (A + B + C) / 2;
+	float T = (A * B * C) / (4 * sqrt(P * (P - A) * (P - B) * (P - C)));
+	float Result = PI * pow(T, 2);
+	return Result;
+}
+
+void PrintCircleAreaArbitraryTriangle(float CircleArea)
+{
+	cout << "\n====================================================\n";
+	cout << "The Circle Area as Arbitrary Triangle  : " << CircleArea << endl;
+	cout << "====================================================\n";
+}
+
+
+int main()
+{
+	float A, B,C;
+	ReadRightSideBase(A, B,C);
+	PrintCircleAreaArbitraryTriangle(CalcuCircleAreaTriangle(A,B,C));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+// Problem_24 >> Validate Age Between 18 and 45
+// Write a program to ask the user enter Age
+// - If age is between 18 and 45 print "Valid Age" otherwise print "Invalid Age"
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadAge ()
+{
+	cout << "Write a program to ask the user enter Age If age is between 18 and 45 print Valid Age otherwise print Invalid Age" << endl << endl;
+	int Age;
+	cout << "Enter Your Age : ";
+	cin >> Age;
+	return Age;
+}
+bool CheckValidAge(int Age , int From , int To)
+{
+	return (Age >= From && Age <= To);
+}
+
+void PrintValideAge(int Age)
+{
+	if (CheckValidAge(Age, 18, 45))
+	{
+		cout << "\n========================\n";
+		cout << Age << " Is Valid Age" << endl;
+		cout << "========================\n";
+	}
+	else
+	{
+		cout << "\n========================\n";
+		cout << Age << " Is InValid Age" << endl;
+		cout << "========================\n";
+	}
+}
+
+int main()
+{
+	PrintValideAge(ReadAge());
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+// Problem_25 >> Read until Age between 18 and 45
+// Write a program to ask the user to enter : age if age is 18 and 45 print "Valid Age" otherwise print "Invalid Age" and re - ask user to enter a valid age .
+// note : you should keep asking user to enter a valid age until s / she enters it .
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int ReadAge ()
+{
+	//cout << "Write a program to ask the user to enter : age if age is 18 and 45 print Valid Age otherwise print Invalid Age and re - ask user to enter a valid age" << endl << endl;
+	int Age;
+	cout << "Enter Your Age : ";
+	cin >> Age;
+	return Age;
+}
+
+bool CheckValidAge(int Age , int From , int To)
+{
+	return (Age >= From && Age <= To);
+}
+
+int KeepAskingUser(int From , int To)
+{
+	int Age = 0;
+	do
+	{
+		Age = ReadAge(); 
+	} while (!CheckValidAge(Age , From , To));
+	return Age;
+}
+
+void PrintValideAge(int Age)
+{
+	cout << "Your Age is  " << Age << " Valid Age " << endl;
+}
+
+int main()
+{
+	
+	PrintValideAge(KeepAskingUser(18,45));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
 
