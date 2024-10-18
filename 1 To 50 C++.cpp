eae29c8e24c2b927d3e3984c7e698b51ef0a9c1d
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-18 06:33:59 +0300

    Update 1 To 50 C++.cpp

--- 1 To 50 C++.cpp
+++ 1 To 50 C++.cpp
@@ -427,3 +427,265 @@ int main()
 //================================================================================================================================================================
 //================================================================================================================================================================
 
+// Problem_11 >> Write a program to ask the user to enter : Mark1 , Mark2 , Mark3 Then Print the Average of entered Marks , and print "Pass" if average >= 50 , otherwise print "Fail"
+
+#include <iostream>
+using namespace std;
+
+enum enStudentsResult { Pass = 1 , Fail = 2};
+
+void ReadMarks(float& Mark1, float& Mark2, float& Mark3)
+{
+	cout << "Write a program to ask the user to enter : Mark1 , Mark2 , Mark3 Then Print the Average of entered Marks , and print Pass if average >= 50 , otherwise print Fail" << endl << endl;
+
+	cout << "Enter Mark 1 : ";
+	cin >> Mark1;
+
+	cout << "Enter Mark 2 : ";
+	cin >> Mark2;
+
+	cout << "Enter Mark 3 : ";
+	cin >> Mark3;
+}
+
+float SumMarks(float Mark1, float Mark2, float Mark3)
+{
+	return Mark1 + Mark2 + Mark3;
+}
+
+float AvergeCalc(float Mark1, float Mark2, float Mark3)
+{
+	return SumMarks(Mark1, Mark2, Mark3) / 3;
+}
+
+enStudentsResult CheckPassFail(float Result)
+{
+	if (Result >= 50)
+		return enStudentsResult::Pass;
+	else
+		return enStudentsResult::Fail;
+}
+
+
+void PrintResutAverge(float Result)
+{
+	if (Result == enStudentsResult::Pass)
+	{
+		cout << "\n=================================\n";
+		cout << "Your Score Is Pass " << endl;
+		cout << "\n=================================\n";
+	}
+	else
+	{
+		cout << "\n=================================\n";
+		cout << "Your Score Is Fail " << endl;
+		cout << "=================================\n";
+	}
+}
+
+int main()
+{
+	float Mark1, Mark2, Mark3;
+	ReadMarks(Mark1, Mark2, Mark3);
+	PrintResutAverge(CheckPassFail(AvergeCalc(Mark1, Mark2, Mark3)));
+
+	return 0;
+}
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+// Problem_12 >> Write a program to ask the user to enter : (Number1 , Number2) , Then print the max Number.
+
+
+#include <iostream>
+using namespace std;
+
+void ReadNumbers(float &Number1, float &Number2)
+{
+	cout << "Write a program to ask the user to enter : (Number1 , Number2) , Then print the max Number" << endl << endl;
+
+	cout << "Enter Number 1 : ";
+	cin >> Number1;
+
+	cout << "Enter Number 2 : ";
+	cin >> Number2;
+}
+
+float MaxNumber(float Number1, float Number2)
+{
+	if (Number1 > Number2)
+		return Number1;
+	else
+		return Number2;
+}
+
+void PrintMaxNumber(float MaxNum)
+{
+	cout << "\n==================\n";
+	cout << "The Max Of Number is : " << MaxNum << endl;
+	cout << "==================\n";
+}
+
+
+int main()
+{
+	float Number1, Number2;
+	ReadNumbers(Number1, Number2);
+	PrintMaxNumber(MaxNumber(Number1, Number2));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+// Problem_13 >> Write a program to ask the user to enter 3 numbers : A , B , C , Then print Max number
+
+
+#include <iostream>
+using namespace std;
+
+void ReadNumbers(float &NumOfA, float &NumOfB, float & NumOfC)
+{
+	cout << "Write a program to ask the user to enter 3 numbers : A , B , C , Then print Max number" << endl << endl;
+
+	cout << "Enter Number 1 : ";
+	cin >> NumOfA;
+
+	cout << "Enter Number 2 : ";
+	cin >> NumOfB;
+
+	cout << "Enter Number 2 : ";
+	cin >> NumOfC;
+}
+
+float MaxNumbers(float NumOfA, float NumOfB , float NumOfC)
+{
+	if (NumOfA > NumOfB)
+		if (NumOfA > NumOfC)
+			return NumOfA;
+		else
+			return NumOfC;
+		else
+			if (NumOfB > NumOfC)
+				return NumOfB;
+			else
+				return NumOfC;
+}
+
+void PrintMaxNumber(float MaxNumber)
+{
+	cout << "\n==================\n";
+	cout << "The Max Of Number is : " << MaxNumber << endl;
+	cout << "==================\n";
+}
+
+
+int main()
+{
+	float NumOfA, NumOfB , NumOfC;
+	ReadNumbers(NumOfA, NumOfB, NumOfC);
+	PrintMaxNumber(MaxNumbers(NumOfA, NumOfB, NumOfC));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+// Problem_14 >> Swap Numbers Write a program to ask the user to user : Number1 . Number2  ,Then print two numbers , then swap the two numbers and print them 
+
+#include <iostream>
+using namespace std;
+
+void ReadNumbers(int &Number1, int &Number2)
+{
+	cout << " Swap Numbers Write a program to ask the user to user : Number1 . Number2  ,Then print two numbers , then swap the two numbers and print them " << endl << endl;
+
+	cout << "Enter Number 1  : ";
+	cin >> Number1;
+
+	cout << "Enter Number 2  : ";
+	cin >> Number2;
+}
+
+//by reference 
+void SwapNumbers(int &Number1,int &Number2)
+{
+	int Temp;
+	Temp = Number1;
+	Number1 = Number2;
+	Number2 = Temp;
+}
+
+void PrintNumber(int Number1, int Number2)
+{
+	cout << "\n==============================================\n";
+	cout << "Number One is " << Number1 << " And Number Two is : " << Number2 << endl;
+	cout << "==============================================\n";
+}
+
+int main()
+{
+	int Number1, Number2;
+	ReadNumbers(Number1, Number2);
+	PrintNumber(Number1, Number2);
+
+	SwapNumbers(Number1, Number2);
+	PrintNumber(Number1, Number2);
+
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
+
+//Problem_15 >>Rectangle Area Write a program to calculate rectangle area and print it on the screen 
+// Area = a*b 
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void ReadLengthWidth(float &A , float &B)
+{
+	cout << "Rectangle Area Write a program to calculate rectangle area and print it on the screen  , Area = a*b" << endl << endl;
+	
+	cout << "Enter The Length : ";
+	cin >> A;
+
+	cout << "Enter The Width : ";
+	cin >> B;
+}
+
+float CalcRectangleArea(float A, float B)
+{
+	return A * B;
+}
+
+void PrintResultArea(float RectangleArea)
+{
+	cout << "\n==========================\n";
+	cout << "The Rectangle Area is : " << RectangleArea << endl;
+	cout << "\n==========================\n";
+}
+
+int main()
+{
+	float A, B;
+	ReadLengthWidth(A, B);
+	PrintResultArea(CalcRectangleArea(A, B));
+	return 0;
+}
+
+
+//================================================================================================================================================================
+//================================================================================================================================================================
+//================================================================================================================================================================
