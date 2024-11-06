a061389589f31f64430caf67ab28d6db1fa96791
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-11-06 20:14:53 +0200

    Create Problem_1.cs

---
+++ Problem_1.cs
@@ -0,0 +1,12 @@
+//Problem_1 >> Write a program to print your name on screen 
+namespace _1to_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Name = "Abdelwahab Shandy";
+            Console.WriteLine($"Your Name Is : {Name}");
+        }
+    }
+}
