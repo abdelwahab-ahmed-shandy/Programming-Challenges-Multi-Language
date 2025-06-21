1748511199365a5a670792d58dc764219d2d29fc
Abdelwahab Shandy 🦅 <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-06-21 04:55:28 +0300

    Update 06-Problem.cpp Review And Add A New Solution

--- Programming Challenges With CPP/00-CPlusPlus-Flowcharts-Level-One-50-Problem/06-Problem.cpp
+++ Programming Challenges With CPP/00-CPlusPlus-Flowcharts-Level-One-50-Problem/06-Problem.cpp
@@ -34,3 +34,67 @@ int main()
 	PrintPersonalInfo(ReadPersonlInfo());
 	return 0;
 }
+
+//=====================================================================================================================================
+//=====================================================================================================================================
+//=====================================================================================================================================
+
+/*
+* 
+* Problem_6 
+* 
+* Write a program to ask user to enter : first name amd last name
+* 
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct stInfoPerson
+{
+    string firstName;
+	string lastName;
+};
+
+string enterYourFirstName()
+{
+    string firstName;
+    cout << "Enter Your First Name : ";
+    getline(cin, firstName);
+    
+    return firstName;
+}
+
+string enterYourLastName()
+{
+    string lastName;
+    cout << "Enter Your Last Name : ";
+    getline(cin, lastName);
+
+    return lastName;
+}
+
+stInfoPerson ReadInfo()
+{
+    stInfoPerson Info;
+
+    Info.firstName = enterYourFirstName();
+
+    Info.lastName = enterYourLastName();
+
+    return Info;
+}
+
+void PrintInfoPerson(stInfoPerson Info)
+{
+    cout << "\n Your Full Name Is : " << Info.firstName << Info.lastName << endl;
+}
+
+
+int main()
+{
+    PrintInfoPerson(ReadInfo());
+    return 0;
+}
